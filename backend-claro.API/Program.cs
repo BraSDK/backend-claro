2f5ca872f7497f454e4c5be523442183b4799007
BraSDK <brasusano@gmail.com>
2026-08-26 11:33:15 -0500

    Configuracion inicial de Token JWT - Authentication

--- backend-claro.API/Program.cs
+++ backend-claro.API/Program.cs
@@ -2,12 +2,54 @@ using Microsoft.EntityFrameworkCore;
 using backend_claro.Infrastructure.Persistence;
 using backend_claro.Application.Interfaces;
 
+// JWT Y SWAGGER
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Swagger para aceptar el TOKEN
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Backend Claro API", Version = "v1" });
+
+    // Definir Esquema de seguridad (boton Authorize)
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Autorización JWT usando el esquema Bearer. \r\n\r\n Escribe 'Bearer' [espacio] y luego tu token en la caja de texto.\r\n\r\nEjemplo: 'Bearer eyJhbG...'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
+//builder.Services.AddSwaggerGen();
 
 // 1. Configurar Entity Framework con PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -15,7 +57,29 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 builder.Services.AddScoped<IAuthService, backend_claro.Infrastructure.Services.AuthService>();
-builder.Services.AddControllers();
+
+// CONFIGURAR LA VALIDACIÓN DEL TOKEN JWT
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettings["SecretKey"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!))
+    };
+});
 
 var app = builder.Build();
 
@@ -27,6 +91,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
--- backend-claro.Infrastructure/Services/AuthService.cs
+++ backend-claro.Infrastructure/Services/AuthService.cs
@@ -2,17 +2,24 @@ using Microsoft.EntityFrameworkCore;
 using backend_claro.Application.Interfaces;
 using backend_claro.Application.DTOs.Auth;
 using backend_claro.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
 
 namespace backend_claro.Infrastructure.Services;
 
 public class AuthService : IAuthService
 {
     private readonly IApplicationDbContext _context;
+    private readonly IConfiguration _configuration;
 
     // Inyectamos tu contexto de base de datos
-    public AuthService(IApplicationDbContext context)
+    public AuthService(IApplicationDbContext context, IConfiguration configuration)
     {
         _context = context;
+        _configuration = configuration;
     }
 
     public async Task<string> RegisterAsync(RegisterRequestDto request)
@@ -70,6 +77,39 @@ public class AuthService : IAuthService
         }
 
         // 3. Por ahora devolvemos un texto simulado. En el próximo paso generaremos el Token Real.
-        return "Simulacion_De_Token_JWT_Valido_123456";
+        return GenerarTokenJwt(cuenta);
+    }
+
+    // Metodo provado para fabricar el Token
+    private string GenerarTokenJwt(CuentaUsuario cuenta)
+    {
+        // Leer las variables es appsettings.json
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+        var secretKey = jwtSettings["secretKey"];
+        var key = Encoding.ASCII.GetBytes(secretKey!);
+
+        // Crear los Claims (la imformacion que viajara dentro del token)
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, cuenta.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, cuenta.Email),
+            new Claim("rol", cuenta.Rol) //Guardamos el rol para futuras validaciones
+        };
+
+        // Configurar los detalles del token (firma, expiracion, etc.)
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(int.Parse(jwtSettings["ExpirationInMinutes"]!)),
+            SigningCredentials =  new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            Issuer = jwtSettings["Issuer"],
+            Audience = jwtSettings["Audience"]
+        };
+
+        // Crear y devolver el token final
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
     }
 }
