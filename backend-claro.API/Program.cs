e6d904040b7f6268df01203d56f55ad69e3d413e
BraSDK <brasusano@gmail.com>
2026-09-09 17:40:53 -0500

    Feature: Authorizacion de CORS para el front - Program.cs

--- backend-claro.API/Program.cs
+++ backend-claro.API/Program.cs
@@ -4,6 +4,29 @@ using backend_claro.API.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+// Configuración de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirFrontend", policy =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            policy.AllowAnyOrigin() // El puerto exacto de tu React
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+        }
+        else
+        {
+            // En el servidor real
+            policy.WithOrigins("https://www.tudominio-claro.com")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        }
+        
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerDocumentation();
@@ -19,6 +42,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Aplicar la política de CORS
+app.UseCors("PermitirFrontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
